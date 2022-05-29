ec5bfab5de57669f6be629ed859b9c2e4d7d5f59
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 09:39:44 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,35 +1,40 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/osx/os_kext_return.hpp>
 
-TEST_CASE("os_kext_return") {
-  {
-    pqrs::osx::os_kext_return r(kOSReturnSuccess);
-    REQUIRE(r.to_string() == "kOSReturnSuccess");
-    REQUIRE(r == true);
-    REQUIRE(r.success() == true);
-    REQUIRE(r.system_policy() == false);
-  }
-  {
-    pqrs::osx::os_kext_return r(kOSKextReturnInternalError);
-    REQUIRE(r.to_string() == "kOSKextReturnInternalError");
-    REQUIRE(r == false);
-    REQUIRE(r.success() == false);
-    REQUIRE(r.system_policy() == false);
-  }
-  {
-    pqrs::osx::os_kext_return r(kOSKextReturnSystemPolicy);
-    REQUIRE(r.to_string() == "kOSKextReturnSystemPolicy");
-    REQUIRE(r == false);
-    REQUIRE(r.success() == false);
-    REQUIRE(r.system_policy() == true);
-  }
-  {
-    pqrs::osx::os_kext_return r(-603946990);
-    REQUIRE(r.to_string() == "kOSKextReturnBootLevel");
-    REQUIRE(r == false);
-    REQUIRE(r.success() == false);
-    REQUIRE(r.system_policy() == false);
-  }
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "os_kext_return"_test = [] {
+    {
+      pqrs::osx::os_kext_return r(kOSReturnSuccess);
+      expect(r.to_string() == "kOSReturnSuccess");
+      expect(r == true);
+      expect(r.success() == true);
+      expect(r.system_policy() == false);
+    }
+    {
+      pqrs::osx::os_kext_return r(kOSKextReturnInternalError);
+      expect(r.to_string() == "kOSKextReturnInternalError");
+      expect(r == false);
+      expect(r.success() == false);
+      expect(r.system_policy() == false);
+    }
+    {
+      pqrs::osx::os_kext_return r(kOSKextReturnSystemPolicy);
+      expect(r.to_string() == "kOSKextReturnSystemPolicy");
+      expect(r == false);
+      expect(r.success() == false);
+      expect(r.system_policy() == true);
+    }
+    {
+      pqrs::osx::os_kext_return r(-603946990);
+      expect(r.to_string() == "kOSKextReturnBootLevel");
+      expect(r == false);
+      expect(r.success() == false);
+      expect(r.system_policy() == false);
+    }
+  };
+
+  return 0;
 }
