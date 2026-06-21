41ae1c8a2eba92f8a1dfadaad3ad4bd3484c7b5a
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-21 10:43:51 +0900

    v2.1.0

--- include/pqrs/osx/os_kext_return.hpp
+++ include/pqrs/osx/os_kext_return.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::os_kext_return v2.0
+// pqrs::osx::os_kext_return v2.1.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
